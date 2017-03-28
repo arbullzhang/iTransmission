47fc03e80e1f925f9dd5b4b616ab8b6b0d624605
arbullzhang <arbullzhang@gmail.com>
2017-03-28 10:45:36 +0800

    初始化项目。

--- README.md
+++ README.md
@@ -1,2 +1,82 @@
-# iTransmission
-BT下载
+iTransmission 4
+==========
+iTransmission, basically, is a torrent client, which uses libtransmission as it's backend.
+
+So, what is iTransmission 4? iTransmission , originally, was developed by [ccp0101](https://github.com/ccp0101). It is no longer kept active. I plan on keeping iTransmision 4 active.~~, as well as building it for older devices (currently as old as iOS 4/iPT 2).~~
+
+This is an extension to [ioshomebrew's](https://github.com/ioshomebrew) iTransmission 3, which is basically a torrent client for iOS, which I plan on keeping active and adding new features to as per my requirements and requests from you i.e. the user.
+I've already added lots of stuff.
+
+Compiling libraries
+-----
+~~You may wish to compile iTransmission libraries yourself~~
+
+There is no longer a need to compile iTransmission libraries yourself. I've already done that for you. The libraries include armv7, armv7s and arm64. They are in Project/Libraries folder, in zip format.
+
+Yet, if you still want to be adventurous, I have added the required files in a folder named 'Compilation'.
+
+How to compile libraries
+_____
+Open terminal
+cd to Compilation directory
+./build.sh
+If you need to change options, edit the configuration & build.sh file in Compilation directory. 
+
+Compiling app
+-----
+Compiling may be a bit hard, and depends upon your target. I will cover the basics below. I assume that you are using Mac OS X and have XCode.
+You can't compile the app without either of those. But, I've included a pre-compiled app. Use that with Installation Option I or II.
+
+1. The app will never be accepted in the app store. Therefore, we do not need codesigning. But XCode requires us to codesign each and every app. Assuming I use the iOS 5.1 SDK, I can disable this check by opening /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS5.1.sdk/SDKSettings.plist with a text editor (not XCode) and change the following values 
+
+```
+<key>CODE_SIGNING_REQUIRED</key>
+<string>YES</string>
+```
+to 
+```
+<key>CODE_SIGNING_REQUIRED</key>
+<string>NO</string>
+```
+
+
+```
+<key>ENTITLEMENTS_REQUIRED</key>
+<string>YES</string>
+```
+to 
+```
+<key>ENTITLEMENTS_REQUIRED</key>
+<string>NO</string>
+```
+
+2. You may also want to change the minimum deployment target to your choice and the base SDK to your choice also.
+
+Installation
+-----
+Installation is the easiest part.
+You will need a jailbroken iDevice.
+
+**OPTION I**
+
+1. Put the compiled .app in the /Applications folder of your iDevice.
+2. SSH into your device and execute the following command(without double-quotes):"cd /Applica*/iTransm*;chmod 777 ./iTransmission; cd ~/;"
+
+**OPTION II**
+
+1. Make a folder. Name doesn't matter, but using "iTransmission" here.
+2. Open the folder.
+3. Make a folder named "Payload". Name matters here.
+4. Copy the compiled .app into the Payload folder.
+5. Compress(Zip only) the iTransmission folder to iTransmission.zip
+6. Rename iTransmission.zip to iTransmission.ipa
+
+Credits
+-------
+- [Transmission](http://www.transmissionbt.com/)
+- [ccp0101](https://github.com/ccp0101)
+- [ioshomebrew](https://github.com/ioshomebrew)
+- [fopino](https://github.com/fopino)
+- [heavenly-awker](https://github.com/heavenly-awker)
+
+Mail me if I missed somebody.
---
+++ Source/ColoredProgress/PDColoredProgressView.m
@@ -0,0 +1,154 @@
+//
+//  PDColoredProgressView.m
+//  PDColoredProgressViewDemo
+//
+//  Created by Pascal Widdershoven on 03-01-09.
+//  Copyright 2009 Pascal Widdershoven. All rights reserved.
+//  
+
+#import "PDColoredProgressView.h"
+
+static void addRoundedRectToPath(CGContextRef context, CGRect rect, float ovalWidth,
+								 float ovalHeight)
+{
+	float fw, fh;
+	// If the width or height of the corner oval is zero, then it reduces to a right angle,
+	// so instead of a rounded rectangle we have an ordinary one.
+	if (ovalWidth == 0 || ovalHeight == 0) {
+		CGContextAddRect(context, rect);
+		return;
+	}
+	
+	//  Save the context's state so that the translate and scale can be undone with a call
+	//  to CGContextRestoreGState.
+	CGContextSaveGState(context);
+	
+	//  Translate the origin of the contex to the lower left corner of the rectangle.
+	CGContextTranslateCTM(context, CGRectGetMinX(rect), CGRectGetMinY(rect));
+	
+	//Normalize the scale of the context so that the width and height of the arcs are 1.0
+	CGContextScaleCTM(context, ovalWidth, ovalHeight);
+	
+	// Calculate the width and height of the rectangle in the new coordinate system.
+	fw = CGRectGetWidth(rect) / ovalWidth;
+	fh = CGRectGetHeight(rect) / ovalHeight;
+	
+	// CGContextAddArcToPoint adds an arc of a circle to the context's path (creating the rounded
+	// corners).  It also adds a line from the path's last point to the begining of the arc, making
+	// the sides of the rectangle.
+	CGContextMoveToPoint(context, fw, fh/2);  // Start at lower right corner
+	CGContextAddArcToPoint(context, fw, fh, fw/2, fh, 1);  // Top right corner
+	CGContextAddLineToPoint(context, 0, fh); // Top left corner
+	CGContextAddLineToPoint(context, 0, 0); // Top left corner
+
+//	CGContextAddArcToPoint(context, 0, fh, 0, fh/2, 1); // Top left corner
+//	CGContextAddArcToPoint(context, 0, 0, fw/2, 0, 1); // Lower left corner
+	
+	CGContextAddArcToPoint(context, fw, 0, fw, fh/2, 1); // Back to lower right
+	
+	// Close the path
+	CGContextClosePath(context);
+	
+	// Restore the context's state. This removes the translation and scaling
+	// but leaves the path, since the path is not part of the graphics state.
+	CGContextRestoreGState(context);
+}
+
+static void fillRectWithLinearGradient(CGContextRef context, CGRect rect, CGFloat colors[], int numberOfColors, CGFloat locations[]) {
+	CGContextSaveGState(context);
+	
+	if(!CGContextIsPathEmpty(context))
+		CGContextClip(context);
+	
+	CGContextTranslateCTM(context, CGRectGetMinX(rect), CGRectGetMinY(rect));
+	CGPoint start = CGPointMake(0, 0);
+	CGPoint end = CGPointMake(0, rect.size.height);
+	
+	CGColorSpaceRef space = CGColorSpaceCreateDeviceRGB();
+    CGGradientRef gradient = CGGradientCreateWithColorComponents(space, colors, locations, numberOfColors);
+	CGContextDrawLinearGradient(context, gradient, end, start, 0);
+	CGContextRestoreGState(context);
+	
+	CGColorSpaceRelease(space);
+	CGGradientRelease(gradient);
+}
+
+@implementation PDColoredProgressView
+@synthesize progress = _progress;
+
+- (id) initWithCoder: (NSCoder*)aDecoder {
+	if(self=[super initWithCoder: aDecoder]) {
+		[self setTintColor: [UIColor colorWithRed: 43.0/255.0 green: 134.0/255.0 blue: 225.0/255.0 alpha: 1]];
+	}
+	return self;
+}
+
+- (void)setProgress:(CGFloat)progress
+{
+    _progress = progress;
+    [self setNeedsDisplay];
+}
+
+- (void)drawRect:(CGRect)rect {
+    
+		CGContextRef ctx = UIGraphicsGetCurrentContext();
+		
+		// draw rectangle
+        if(!CGContextIsPathEmpty(ctx))
+        {
+            CGContextClip(ctx);
+        }
+		
+        // fill empty progress bar with white
+		CGContextSetRGBFillColor(ctx, 102.0f/255.0f, 102.0f/255.0f, 102.0f/255.0f, 1);
+		CGContextFillRect(ctx, rect);
+		
+		// fill upperhalf with light grey
+		CGRect upperhalf = rect;
+		upperhalf.size.height /= 1.75;
+		upperhalf.origin.y = 0;
+		
+		CGRect progressRect = rect;
+		progressRect.size.width *= [self progress];
+		
+        if(!CGContextIsPathEmpty(ctx))
+        {
+            CGContextClip(ctx);
+        }
+		
+		CGContextSetFillColorWithColor(ctx, [_tintColor CGColor]);
+		CGContextFillRect(ctx, progressRect);
+		
+		progressRect.size.width -= 1.25;
+		progressRect.origin.x += 0.625;
+		progressRect.size.height -= 1.25;
+		progressRect.origin.y += 0.625;
+}
+
+- (void) setTintColor: (UIColor *) aColor {
+	_tintColor = aColor;
+	[self setNeedsDisplay];
+}
+
+- (void)useGreenColor
+{
+	[self setTintColor:[UIColor colorWithRed:112.0f/255.0f green:221.0f/255.0f blue:129.0f/255.0f alpha:1.0f]];
+}
+
+- (void)useBlueColor
+{
+	[self setTintColor:[UIColor colorWithRed:98.0f/255.0f green:183.0f/255.0f blue:240.0f/255.0f alpha:1.0f]];
+}
+
+- (void)useBlackColor
+{
+    [self setTintColor:[UIColor colorWithRed:255.0f green:255.0f blue:255.0f alpha:1.0f]];
+}
+
+- (void)useWhiteColor
+{
+    [self setTintColor:[UIColor colorWithRed:0.0f green:0.0f blue:0.0f alpha:1.0f]];
+    NSLog(@"Using white");
+}
+
+@end
